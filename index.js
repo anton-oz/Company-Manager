88ca6a4ebd41cbc53c3a1fc08f50897a1b4eeb4b
anton <antonosland789@gmail.com>
2024-06-01 15:10:08 -0500

    View all for each table working properly

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// TO DO
-// Refactor all code that relates to 'company_info' database to work with included .sql files
-
 const {startApplication} = require("./lib/inqPrompts");
 
 startApplication();
