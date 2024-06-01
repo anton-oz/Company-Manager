1daaa44a7637453cce01d1f0b18305298879cc6d
anton <antonosland789@gmail.com>
2024-05-31 20:08:38 -0500

    refactored code relating to database, sql files updated, added tableMaker function to render tables, now working on getting pg queries to display proper info

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,34 @@
 -- delete existing company_info database and create a new one
 DROP DATABASE IF EXISTS company_info;
 CREATE DATABASE company_info;
+
+-- Create Tables 
+-- Department table
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+-- Role table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+);
+-- Employee table
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);
--- index.js
+++ index.js
@@ -1,50 +1,6 @@
 // TO DO
 // Refactor all code that relates to 'company_info' database to work with included .sql files
 
-const color = require("colors");
+const {startApplication} = require("./lib/inqPrompts");
 
-const inquirer = require("inquirer");
-
-const {Pool} = require("pg");
-
-//const createDb = require("./lib/createDb");
-
-
-inquirer.prompt([
-    {
-        type: 'confirm',
-        message: color.yellow.bold.bgBlack(`If you have any database named company_info, this app will delete all 
-previous information for that database. Please confirm that you understand.`),
-        name: 'continue'
-    }
-])
-.then(async (answer) => {
-    let runningVariable = 1;
-
-    while (runningVariable === 1) {
-        if (runningVariable === 1) {
-            await inquirer.prompt([
-                {
-                    type: 'list',
-                    message: 'What would you like to do?',
-                    choices: [
-                        'View all departments', 
-                        'View all roles', 
-                        'View all employees', 
-                        'Add new department', 
-                        'Add new role', 
-                        'Add new employee', 
-                        'Update employee role',
-                        'Quit Company Manager'
-                    ],
-                    name: 'selected'
-                }
-            ])
-            .then((option) => {
-                if (option.selected === 'Quit Company Manager') {
-                    return runningVariable = 0
-                }
-            })
-        } 
-    };
-});
+startApplication();
